f558656b94195202558f4049f47eff0d78bd121a
Fernanndo Miguel <fernnandomiguel@gmail.com>
2026-05-28 15:19:00 -0300

    refactor: organiza declarações das funções no header

---
+++ gastos.h
@@ -0,0 +1,11 @@
+#ifndef GASTOS_H
+#define GASTOS_H
+
+void adicionar_gasto();
+void listar_gasto();
+void mostrar_total();
+void salvar_gastos();
+void carregar_gastos();
+void remover_gasto();
+
+#endif
